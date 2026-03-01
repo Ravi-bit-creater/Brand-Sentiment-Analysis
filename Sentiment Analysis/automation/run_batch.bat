9a59aa0d058fc881eb0ce5f79b381f0e0f4e97a5
Ravi-bit-creater <ravimanikandanm@gmail.com>
2026-03-01 19:20:00 +0530

    Add files via upload

---
+++ Sentiment Analysis/automation/run_batch.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\Admin\OneDrive\data pattern\Sentiment Analysis\automation"
+python run_batch.py
---
+++ Sentiment Analysis/automation/run_daily_report.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\Admin\OneDrive\data pattern\Sentiment Analysis\reports"
+python generate_daily_report.py
